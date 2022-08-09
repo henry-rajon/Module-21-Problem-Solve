726536dff1c8d1db983b7341009b6a2c59015c48
Raj_Dash <rajdash3472@gmail.com>
2022-08-09 10:08:57 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,3 @@
+// 1. What is immutable string ?
+    
+//Ans : In Java, String objects are immutable.Immutable simply means unmodifiable or unchangeable.Once String object is created its data or state can't be changed but a new String object is created.
